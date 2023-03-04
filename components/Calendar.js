37a650c413d5aa2e343a9833d0ef1290eea90889
Sergei Barsukov <sergforweb@gmail.com>
2023-03-04 12:00:20 +0300

    fix errors

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import googleCalendarPlugin from '@fullcalendar/google-calendar'
 
 export default class GoogleCalendar extends React.Component {
   render() {
     return (
       <FullCalendar
-        plugins={[dayGridPlugin, googleCalendarPlugin]}
+        plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
         events={[
           { googleCalendarId: 'sergforweb@gmail.com' }
