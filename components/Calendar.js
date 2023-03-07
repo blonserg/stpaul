330469a4b6bcc380889182d442ebf8944d55a996
Sergei Barsukov <sergforweb@gmail.com>
2023-03-07 21:03:10 +0300

    fix events at calendar

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
 
 export default class GoogleCalendar extends React.Component {
   render() {
     return (
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, googleCalendarPlugin]}
         initialView="dayGridMonth"
-        events={[
+        googleCalendarApiKey='AIzaSyBP7bvd-lBWnGxTUBggBjAYLb6GJy6T9iU'
+        events={
           { googleCalendarId: 'sergforweb@gmail.com' }
-        ]}
+        }
       />
     )
   }
