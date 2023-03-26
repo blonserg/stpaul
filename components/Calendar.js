aa88a54eea20417966f22757bea7e6fd8a57fd61
Sergei Barsukov <sergforweb@gmail.com>
2023-03-26 11:14:28 +0300

    change calendar id

--- components/Calendar.js
+++ components/Calendar.js
@@ -11,7 +11,7 @@ export default class GoogleCalendar extends React.Component {
         initialView="dayGridMonth"
         googleCalendarApiKey='AIzaSyBP7bvd-lBWnGxTUBggBjAYLb6GJy6T9iU'
         events={
-          { googleCalendarId: 'sergforweb@gmail.com' }
+          { googleCalendarId: '8970c777ae0b4fde0126e28b5189b90777acbadd256c05b3f2635b04f3d1ee70@group.calendar.google.com' }
         }
       />
     )
